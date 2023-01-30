f5283745503eec85daf4811bbbdeae3a6a334357
Alex <alexs@makebetter.co.za>
2023-01-30 16:27:49 +0200

    new

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,49 @@
+import { useNavigation } from "@react-navigation/native";
+import { FlatList, View, Text, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
+import PlaceItem from "./PlaceItem";
+
+
+function PlacesList({places}) {
+    const navigation = useNavigation();
+
+    function selectPlaceHandler(id) {
+        navigation.navigate('PlaceDetails', {
+            placeId: id
+        })
+    }
+
+    if (!places || places.length === 0) {
+        return (
+            <View style={styles.fallbackContainer}>
+                <Text style={styles.fallbackText}>no places added yet - add some</Text>
+            </View>
+        )
+    }
+
+    return (
+        <FlatList 
+            style={styles.list}
+            data={places} 
+            keyExtractor={(item) => item.id } 
+            renderItem={({item}) => <PlaceItem place={item} onSelect={selectPlaceHandler} />} 
+        />
+    )
+}
+
+export default PlacesList;
+
+const styles = StyleSheet.create({
+    list: {
+        margin: 24,
+    },
+    fallbackContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    fallbackText: {
+        fontSize: 16,
+        color: Colors.primary200
+    },
+})
